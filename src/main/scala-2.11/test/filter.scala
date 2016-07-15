c89336c60bfe65fd334131cf79bfdc096fdee6f3
xueweili1993 <xueweili1993@gmail.com>
2016-07-15 16:16:50 +0800

    7-15-22

--- src/main/scala-2.11/test/filter.scala
+++ src/main/scala-2.11/test/filter.scala
@@ -45,7 +45,7 @@ object filter {
         .filter{case line =>
 
           val linearray = line.split(" ")
-          line.length<31 && line.length>5
+          line.length<31 && line.length>2 && linearray.length>1
         }
       .distinct()
       .collect
