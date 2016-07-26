6cd84a2ca76082cf9019a8109a989281afb2dc0a
xueweili1993 <xueweili1993@gmail.com>
2016-07-26 10:55:33 +0800

    7-26-1

--- src/main/scala-2.11/test/LDA.scala
+++ src/main/scala-2.11/test/LDA.scala
@@ -30,58 +30,59 @@ object myLDA {
 
 
     val savepath = "hdfs:///lxw/test1"
-    val hdfspath = "hdfs:///lxw/awsdata/*"
+    val hdfspath = "hdfs:///lxw/fuzzymatch/20160623/*"
+    val stoppath = "hdfs:///lxw/stopwords"
     HDFS.removeFile(savepath)
 
+    val stopwords = sc.textFile(stoppath)
+      .collect()
+      .toSet
+
+    val bStop = sc.broadcast(stopwords)
 
     val adlog = sc.textFile(hdfspath)
+      .flatMap{case line=>
 
-      .flatMap {case line =>
+          val linearray = line.split("\t")
+          val text ={
+            if(linearray.length>2)
+              linearray(2)
+            else
+              ""
+          }
 
-        val linearray = line.replaceAll("\\(|\\)","").split(",",4)
-        if (linearray.length>3 && linearray(3)!="")
-          Some((linearray(0),linearray(1)),linearray(3))
-        else
-          None
+          val stop  = bStop.value
+          val words = text.split(" ")
+
+          words.map{x=>
+
+            var stemmer = new Stemmer()
+            stemmer.add(x.trim())
+            if ( stemmer.b.length > 2 )
+            {
+              stemmer.step1()
+              stemmer.step2()
+              stemmer.step3()
+              stemmer.step4()
+              stemmer.step5a()
+              stemmer.step5b()
+            }
+            val x1 = stemmer.b
+
+            if (stop.contains(x1))
+              {
+                None
+              }
+            else{
+              Some((linearray(0),x1))
+            }
+
+
+          }
       }
-      .reduceByKey(_+","+_)
-      .flatMap { case (id, text)=>
-
-        val newtext = text.replaceAll("\\pP|\\pS"," ").replaceAll(" +"," ")
-        newtext.toLowerCase.split(" ")map { x =>
-
-          ((id, x),1)
-        }
-      }
-      .reduceByKey(_+_)
-      .map { case ((id, x), num) =>
-
-        (id,(x,num))
-      }
-      .groupByKey()
-
-      val nums = adlog.map{case (id,array)=>
-
-        val vec = Vectors.dense(array.map(x=>x._2.toDouble).toArray)
-        val words = array.map(x=>x._1).toArray
-        (words,vec)
-      }
-      val pp = nums.map(x=> x._2)
-
-    val corpus = pp.zipWithIndex.map(_.swap).cache()
-
-    val ldaModel = new LDA().setK(3).run(corpus)
-
-    println("Learned topics (as distributions over vocab of " + ldaModel.vocabSize + " words):")
-    //val topics = ldaModel.topicsMatrix
-   /* for (topic <- Range(0, 3)) {
-      print("Topic " + topic + ":")
-      for (word <- Range(0, ldaModel.vocabSize)) { print(" " + topics(word, topic)); }
-      println()
-    }*/
+      .saveAsTextFile(savepath)
 
 
-    //adlog.saveAsTextFile(savepath)
 
   }
 
