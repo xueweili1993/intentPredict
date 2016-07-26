19487991005ef9e71eea813c5fbcaceecd0d849e
xueweili1993 <xueweili1993@gmail.com>
2016-07-26 11:42:22 +0800

    7-26-9

--- src/main/scala-2.11/test/filter.scala
+++ src/main/scala-2.11/test/filter.scala
@@ -98,7 +98,7 @@ object filter {
     val broadtitle = sc.broadcast(title)
 
     println("lxw "+makepath())
-    val mydata = sc.textFile(hdfspath)
+    val mydata = sc.textFile(makepath())
        .flatMap {case line =>
 
          val linearray = line.split("\t")
@@ -109,7 +109,7 @@ object filter {
          }
 
        }
-        .repartition(1000)
+        .repartition(2000)
 
 
 
@@ -154,7 +154,7 @@ object filter {
                 newlist.nonEmpty
         }
 
-        .repartition(1)
+        .repartition(20)
         .saveAsTextFile(savepath)
      // val data2redis=mydata.collect()
 
