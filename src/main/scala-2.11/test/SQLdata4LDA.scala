e403358063f7fe5a45370a062b698aff57d95fbb
xueweili1993 <xueweili1993@gmail.com>
2016-08-08 18:11:13 +0800

    8-8-12

--- src/main/scala-2.11/test/SQLdata4LDA.scala
+++ src/main/scala-2.11/test/SQLdata4LDA.scala
@@ -21,11 +21,11 @@ object SQLdata4LDA {
     val sc = new SparkContext(conf)
     val hadoopConf = sc.hadoopConfiguration
 
-    val catePath = "hdfs:///lxw/test3/part-00003"
+    val catePath = "hdfs:///lxw/test3/part-00004"
 
     val stoppath = "hdfs:///lxw/stopwords"
 
-    val savepath = "hdfs:///lxw/ldaData/part3"
+    val savepath = "hdfs:///lxw/ldaData/part4"
     val descPath = "hdfs:///lxw/AppWithDiscreption/part-00000"
 
     HDFS.removeFile(savepath)
